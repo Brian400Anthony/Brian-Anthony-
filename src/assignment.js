63bfda766bf7d3958416e3be2dc89ab0e4d1fb42
Brian400Anthony <109244045+Brian400Anthony@users.noreply.github.com>
2022-07-13 22:25:31 +0100

    Update assignment.js
    AltSchool JS Assignment done by Brian Anthony Uwe

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+Let sum= 0;
+for (let i= 0; i < arrayOfNumbers.length; i++){ 
+sum+= array0fNumbers[i]
+} 
+   return sum;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +30,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+let number= []
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+for(let i= 0; < array0fNumbers.length; i++){
+ if(array0fNumbers [i] % 2 == 0){
+  number.push(array0fNumbers[i]);
+  };
+ };
+    return numbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
